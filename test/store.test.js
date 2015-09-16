617c7850665e4f9c57e5e5500f6affefafad6e94
Paolo Chiodi <chiodi84@gmail.com>
2015-09-16 10:24:25 +0200

    removed dead code

--- test/store.test.js
+++ test/store.test.js
@@ -1,23 +1,14 @@
 /* Copyright (c) 2014 Richard Rodger, MIT License */
-"use strict";
+'use strict';
 
 
 var seneca = require('seneca')
-
 var shared = require('..')
-
 
 var Lab = require('lab');
 var lab = exports.lab = Lab.script()
 
-var describe = lab.describe
-var it = lab.it
-
 var si = seneca({log:'silent'})
-
-si.__testcount = 0
-var testcount = 0
-
 
 shared.basictest({
   seneca: si,
