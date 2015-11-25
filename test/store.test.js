bd32ad96f50209adba3d3c19633d3703a887e137
Paolo Chiodi <chiodi84@gmail.com>
2015-11-25 17:57:06 +0100

    update seneca dependency and explicitly require mem store

--- test/store.test.js
+++ test/store.test.js
@@ -2,15 +2,23 @@
 'use strict'
 
 var seneca = require('seneca')
+var memStore = require('seneca-mem-store')
 var shared = require('..')
 
 var Lab = require('lab')
 var lab = exports.lab = Lab.script()
 
+var si = seneca({
+  log: 'silent',
+  default_plugins: { 'mem-store': false }
+})
+si.use(memStore)
 
-var si = seneca({ log: 'silent' })
-var merge = seneca({ log: 'silent' })
-merge.use('mem-store', { merge: true })
+var merge = seneca({
+  log: 'silent',
+  default_plugins: { 'mem-store': false }
+})
+merge.use(memStore, { merge: false })
 
 shared.basictest({
   seneca: si,
